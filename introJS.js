076b648acf228cdca803118c41eceeae2c088c91
khadiza21 <bibikhadiza474@gmail.com>
2022-02-01 13:07:36 +0600

    oprations in js and variable declearation

---
+++ introJS.js
@@ -0,0 +1,67 @@
+// to see output in the terminal: node filename.js
+
+// variable declearing
+/* _______________________________________start_______________________________________________________ */
+
+// String type
+var SName = "khadiza";
+
+//numeric type
+var num = 12;
+var fnum = 12.3232;
+
+// boolean type
+// camelcase use in variable name declearation
+var boolType = true;
+
+// print out
+console.log(SName + "\n" + num + "\n" + fnum + "\n" + boolType);
+
+/* __________________________________________end______________________________________________________ */
+
+// operation in javascript
+/* ________________________________________start_______________________________________________________ */
+
+var num1 = 12;
+var num2 = 6;
+var num3 = 14;
+var num4 = 7;
+
+var sum = num1 + num2;
+var sub = num1 - num2;
+var mul = num1 * num2;
+var div = num1 / num2;
+var reminder = num1 % num4;
+
+// Assuming the following variables
+sum += num3;
+
+var greeting = "world";
+var num5 = 5;
+var booll = true;
+
+// Number + Number -> addition
+num5 += 2; // 7
+
+// Boolean + Number -> addition
+booll += 1; // 2
+
+// Boolean + Boolean -> addition
+booll += false; // 1
+
+// Number + String -> concatenation
+num5 += "foo"; // "5foo"
+
+// String + Boolean -> concatenation
+greeting += false; // "foofalse"
+
+// String + String -> concatenation
+greeting += "bar"; // "foobar"
+
+// increment and decrement
+num1++;
+num2--;
+++num3;
+--num4;
+
+/* ______________________________________________end_____________________________________________________ */
