276bf67af6b868af9cc146d5555cb27f80b3b007
khadiza21 <bibikhadiza474@gmail.com>
2022-02-02 13:46:03 +0600

    file rename

---
+++ 4-19-oBjEcT.js
@@ -0,0 +1,18 @@
+// object declearation 
+var student1 = {
+    id:223, //property = (key:value)
+    name:'KA',
+    marks:34, 
+    phone:2323
+};
+
+console.log(student1);
+console.log(student1.id)
+
+var mobile1 = {
+    color:'black',
+    price:13999,
+    screenSize:3,
+    Storage:'28gb'
+  
+};
---
+++ 4-19-sWiTcHbReAk.js
@@ -0,0 +1,10 @@
+
+switch(color){
+    case 'red':
+        console.log('Color is Red');
+        break;
+    case 'yellow':
+        console.log('Color is Yellow');
+    default: 
+    console.log("no color");
+}
