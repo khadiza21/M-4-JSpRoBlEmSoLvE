37b72d08cefce82ddd1d931f81dfba48836bd039
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 20:21:15 +0600

    smallest number of array

--- 4-21-pS/arraySmallElement.js
+++ 4-21-pS/arraySmallElement.js
@@ -1,1 +1,9 @@
+let arr = [12, 9, 10, 23, 15, 234, 45, 987, 23];
 
+let small = arr[0];
+for (let i = 0; i < arr.length; i++) {
+  if (small > arr[i]) {
+    small = arr[i];
+  }
+}
+console.log("Smallest Number of Array ", small);
