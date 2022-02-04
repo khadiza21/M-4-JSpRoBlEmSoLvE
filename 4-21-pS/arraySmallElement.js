e4ead9c333f3ceedb45f236fc5e75aee6a74da6f
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 17:15:04 +0600

    array numbers sum  value calculation

--- 4-21-pS/arraySmallElement.js
+++ 4-21-pS/arraySmallElement.js
@@ -1,10 +1,1 @@
-let arr = [12, 34, 45, 98, 39, 92, 41, 39];
 
-function sumArrayNumbers(array) {
-  let count = 0;
-  for (let i = 0; i < array[i]; i++) {
-    count += array[i];
-  }
-  return count;
-}
-console.log("Array Numbers Sum is ", sumArrayNumbers(arr));
