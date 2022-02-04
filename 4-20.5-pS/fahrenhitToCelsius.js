bac8d2a4083d5853e702567969836bde6a211353
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 16:53:07 +0600

    fahrenhit to celsius  convert

--- 4-20.5-pS/fahrenhitToCelsius.js
+++ 4-20.5-pS/fahrenhitToCelsius.js
@@ -2,7 +2,7 @@
 // fahr = (cels * 9.0/5.0) + 32.0; //Cels to fahr
 
 function fahrenhitToCelsius(fahrenhit) {
-  let celsius = (parseFloat((((fahrenhit - 32.0) * 5.0) / 9.0).toFixed(3)));
+  let celsius = parseFloat((((fahrenhit - 32.0) * 5.0) / 9.0).toFixed(3));
   return celsius;
 }
 console.log("Celsius Value is ", fahrenhitToCelsius(235));
