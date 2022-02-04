226ddceeed4bdc7c17d608e396d6666c11673d87
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 13:36:01 +0600

    is age even or odd checking

--- 4-20-apply-js/isAgeEvenOdd.js
+++ 4-20-apply-js/isAgeEvenOdd.js
@@ -1,1 +1,13 @@
 /* ২. তোমার বয়স কি odd নাকি even সংখ্যা সেটা চেক কর একটা ফাংশন দিয়ে। সেই ফাংশনকে কোন সংখ্যা প্যারামিটার হিসেবে দিলে, সেই সংখ্যা Even হলে ফাংশন true রিটার্ন করবে আর Odd হলে false রিটার্ন করবে। */
+function isAgeEvenOdd(age) {
+  if (age > 0) {
+    if (age % 2 != 0) {
+      return 0;
+    } else if (age % 2 == 0) {
+      return 1;
+    }
+  } else {
+    return "Give Valid Age.";
+  }
+}
+console.log(isAgeEvenOdd(-34));
