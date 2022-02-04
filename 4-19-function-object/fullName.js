34d27bdf7f8e8c767ac2b1e54c3b768eb168b3d0
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 12:49:25 +0600

    area of square calculation

--- 4-19-function-object/fullName.js
+++ 4-19-function-object/fullName.js
@@ -1,6 +1,6 @@
 /* ৩. fullName নামে একটা ফাংশন তৈরী কর যেটা তোমার নামের প্রথম অংশ এবং তোমার নামের শেষের অংশ প্যারামিটার হিসেবে নিবে। আর তোমার নামের দুই অংশ জোড়া দিয়ে আউটপুট হিসেবে তোমার পূর্ন নাম রিটার্ন করে দিবে। যেমন ধরো, hablu এবং kanto ইনপুট প্যারামিটার হিসেবে দিলে আউটপুট হিসেবে hablu kanto রিটার্ন করবে।  */
 
 function fullName(firstName, lastName) {
-  console.log("Fullname is " + firstName + " " + lastName + ".");
+  return "Fullname is " + firstName + " " + lastName + ".";
 }
-fullName("Panthera", "Tigris");
+console.log(fullName("Panthera", "Tigris"));
--- 4-19-function-object/square.js
+++ 4-19-function-object/square.js
@@ -1,5 +1,6 @@
 /* ৪. একটা ফাংশন লিখবা যেটাকে তুমি কোন সংখ্যাকে ইনপুট হিসেবে দিলে সে সেই সংখ্যার square করে সেই square কে রিটার্ন করবে।*/
 function square(side) {
-  console.log("Area of Square ", side * side);
+  area = side * side;
+  return area;
 }
-square(2);
+console.log("Area of Square ", square(12));
