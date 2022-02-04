5dc7d25b065ec33165913f9f7e466c6d3e98f793
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 07:40:08 +0600

    stringBehave explore

--- 4-22-pS/stringBehave.js
+++ 4-22-pS/stringBehave.js
@@ -1,14 +1,13 @@
 let stringBehave = "Bangladesh is Beautiful Country.";
 console.log("The String Length Is ", stringBehave.length);
-console.log("Index of 0: ",stringBehave[0]);
-console.log("Index of 7: ",stringBehave[7]);
-console.log("Index of 19: ",stringBehave[19]);
-console.log("Index of 24: ",stringBehave[24]);
+console.log("Index of 0: ", stringBehave[0]);
+console.log("Index of 7: ", stringBehave[7]);
+console.log("Index of 19: ", stringBehave[19]);
+console.log("Index of 24: ", stringBehave[24]);
 
-// for of loop apply in string 
- let i = 0;
-for (const char of stringBehave){
-   
-    console.log(i,'th index value: ',char);
-    i++;
+// for of loop apply in string
+let i = 0;
+for (const char of stringBehave) {
+  console.log(i, "th index value: ", char);
+  i++;
 }
