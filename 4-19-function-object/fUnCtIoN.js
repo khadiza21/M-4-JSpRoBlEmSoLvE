b7a7f09f2b3afddce558442a5397f77df8c60e87
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 11:25:34 +0600

    function exploring

---
+++ 4-19-function-object/fUnCtIoN.js
@@ -0,0 +1,14 @@
+//add two numbers
+function add(num1, num2) {
+  console.log("number 1 is ", num1, " and number 2 is ", num2);
+  return num1 + num2;
+}
+let adding = add(12 + 28, 80);
+console.log("Addition of two numbers ", adding);
+
+//multifly two numbers
+function multifly(num1, num2) {
+  console.log("number 1 is ", num1, " and number 2 is ", num2);
+  return num1 * num2;
+}
+console.log("Multiplication of two numbers ", multifly(12, 10));
