9b07abff264b0b58a3e381b58a6ddb0b00c412ed
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 17:26:11 +0600

    foober

--- 4-22-pS/fooBar.js
+++ 4-22-pS/fooBar.js
@@ -1,19 +1,14 @@
-function FooBar (n){
-    for (let i = 0; i<=n; i++){
-        if (i%5 == 0 && i%3 == 0){
-            console.log('foobar');
-        }
-        else if (i%3 == 0){
-            console.log('foo');
-        }
-       else if (i%5 == 0){
-            console.log('bar');
-        }
-        
-        else{
-           console.log(i); 
-        }
-        
+function FooBar(n) {
+  for (let i = 0; i <= n; i++) {
+    if (i % 5 == 0 && i % 3 == 0) {
+      console.log("foobar");
+    } else if (i % 3 == 0) {
+      console.log("foo");
+    } else if (i % 5 == 0) {
+      console.log("bar");
+    } else {
+      console.log(i);
     }
+  }
 }
 console.log(FooBar(20));
