ac089bafc32eb9d8535cc39c5f8417f26da0b0b8
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 13:21:31 +0600

    hour to minute convers

--- 4-20-apply-js/hourToMinute.js
+++ 4-20-apply-js/hourToMinute.js
@@ -1,1 +1,6 @@
 /* ৩. এমন একটা ফ্যাংশনা লিখো যেটাকে তুমি ঘন্টাকে ইনপুট প্যারামিটার হিসেবে দিবে। আর সে সেই ঘন্টাকে মিনিটে কনভার্ট করে মিনিট রিটার্ন করবে। */
+function hourToMinute(hour) {
+  let minute = hour * 60;
+  return minute;
+}
+console.log("MINUTE: ", hourToMinute(3));
