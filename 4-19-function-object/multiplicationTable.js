ef7e94ca369d17a59a2604879d70d77a06c69643
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 12:13:35 +0600

    multiplication table

--- 4-19-function-object/multiplicationTable.js
+++ 4-19-function-object/multiplicationTable.js
@@ -1,9 +1,9 @@
 /* ১. একটা ফাংশন লিখবা যেটা ১৩ এর নামতা (multiplication table) আউটপুট হিসেবে দেখাবে।  */
-function multiplicationTable(number){
-    console.log('Multiplication Table of ',number);
-    for(i=1; i<=10; i++){
-        console.log(number,' * ',i,' = ',i*number);
-    }
+function multiplicationTable(number) {
+  console.log("Multiplication Table of ", number);
+  for (i = 1; i <= 10; i++) {
+    console.log(number, " * ", i, " = ", i * number);
+  }
 }
 
 multiplicationTable(12);
