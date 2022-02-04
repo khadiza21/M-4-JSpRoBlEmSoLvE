ae340d6f35a51f8d30054ed71d1713d0c9f24a57
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 17:16:21 +0600

    math bulit in js method explore

--- 4-21-pS/MathMethodExplore.js
+++ 4-21-pS/MathMethodExplore.js
@@ -3,12 +3,9 @@ console.log("Ceiling Value : " + Math.ceil(21.1434));
 console.log("Floor Value : " + Math.floor(23.1434));
 console.log("Round Value : " + Math.round(21.5434));
 console.log("Random Value : " + Math.random() * 1000);
-console.log("Maximum Value : " + Math.max(123,344));
-console.log("Minimum Value : " + Math.min(345,123));
+console.log("Maximum Value : " + Math.max(123, 344));
+console.log("Minimum Value : " + Math.min(345, 123));
 
-
-for(let i=1; i <= 10; i++){
-    
-    console.log(Math.ceil(Math.random()*234));
-   
+for (let i = 1; i <= 10; i++) {
+  console.log(Math.ceil(Math.random() * 234));
 }
