a9267b71dfd647ff2501f24609520a4a8d55127e
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 12:24:17 +0600

    case changer

--- 4-19-function-object/caseChanger.js
+++ 4-19-function-object/caseChanger.js
@@ -1,1 +1,5 @@
 /* ২. একটা ফাংশন লিখবা যেটা যেকোন নামকে uppercase বা রেগুলার কেইস হিসেবে নিবে আর আউটপুট হিসেবে সেই নাম lowercase করে রিটার্ন করবে। */
+function convertLowercase(name) {
+  console.log(name.toLowerCase());
+}
+convertLowercase("PAnTHERA tIgRIS.");
