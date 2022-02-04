66d86ac0d8d6d95a1b490ecaea87049a10a5986f
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 12:32:13 +0600

    pizza

--- 4-19-function-object/pizza.js
+++ 4-19-function-object/pizza.js
@@ -1,15 +1,13 @@
 /* ৫) pizza নামের একটা object কে নিচের উদাহরণ মতো define করবা :
-
-
-
 const pizza = {
-
- toppings: ['cheese', 'sauce', 'pepperoni'],
-
- crust: 'deep dish',
-
- serves: 2
-
+toppings: ['cheese', 'sauce', 'pepperoni'],
+crust: 'deep dish',
+serves: 2
 }
-
 এবং pepperoni প্রিন্ট করবা। */
+const pizza = {
+  toppings: ["cheese", "sauce", "pepperoni"],
+  crust: "deep dish",
+  serves: 2,
+};
+console.log(pizza.toppings[2]);
