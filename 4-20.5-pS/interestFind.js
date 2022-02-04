ed06f8c72c1972c8759fde86381415a41b395bb8
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 16:20:43 +0600

    simple interst calculation

--- 4-20.5-pS/interestFind.js
+++ 4-20.5-pS/interestFind.js
@@ -1,1 +1,11 @@
 // simple interest calculation  formula
+function simpleInterest(
+  principalAmount,
+  annualinterestRate,
+  termOfLoanPerYear
+) {
+  const simpleInterest =
+    (principalAmount * annualinterestRate * termOfLoanPerYear) / 100;
+  return simpleInterest;
+}
+console.log("Your Simple Interest is: ", simpleInterest(500000, 10, 5));
