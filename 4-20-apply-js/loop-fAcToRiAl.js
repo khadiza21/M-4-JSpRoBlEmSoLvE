f66b7fdef2fe459a9ec9ba5c13ee6fc4d806fc18
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 14:27:40 +0600

    factorial

--- 4-20-apply-js/loop-fAcToRiAl.js
+++ 4-20-apply-js/loop-fAcToRiAl.js
@@ -1,1 +1,9 @@
 // function
+function findFactorialNumbers(number) {
+  let fact = 1;
+  for (let i = 1; i <= number; i++) {
+    fact *= i;
+  }
+  console.log(number, " Factorial IS ", fact);
+}
+findFactorialNumbers(6);
--- 4-20-apply-js/whileLoopFactorial.js
+++ 4-20-apply-js/whileLoopFactorial.js
@@ -1,1 +1,13 @@
 //decremental loop
+// function
+function findFactorialNumbers(number) {
+  let fact = 1;
+  let i = number;
+  while (i >= 1) {
+    fact *= i;
+    //console.log(i);
+    i--;
+  }
+  console.log(number, " Factorial IS ", fact);
+}
+findFactorialNumbers(5);
