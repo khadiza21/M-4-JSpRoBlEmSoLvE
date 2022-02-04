efe472cf4a85fe068f43294582feabf205824544
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 12:05:19 +0600

    switch case break default keyword explore

--- 4-19-function-object/sWiTcHbReAk.js
+++ 4-19-function-object/sWiTcHbReAk.js
@@ -1,10 +1,34 @@
+function detectRainbowColor(color) {
+  switch (color) {
+    case "red":
+      console.log("Color is Red");
+      break;
 
-switch(color){
-    case 'red':
-        console.log('Color is Red');
-        break;
-    case 'yellow':
-        console.log('Color is Yellow');
-    default: 
-    console.log("no color");
+    case "orange":
+      console.log("Color is Orange");
+      break;
+
+    case "yellow":
+      console.log("Color is Yellow");
+      break;
+
+    case "green":
+      console.log("Color is Green");
+      break;
+
+    case "sky-blue":
+      console.log("Color is Sky-Blue");
+      break;
+
+    case "blue":
+      console.log("Color is Blue");
+      break;
+    case "purple":
+      console.log("Color is Purple");
+      break;
+    default:
+      console.log("Your Choosen Color isn't Rainbow Color");
+  }
 }
+
+detectRainbowColor("SkY-BlUE".toLowerCase());
