28cb344c041b070f3a2a3fe97dad628c88788d70
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 11:48:48 +0600

    exploring break and continue

---
+++ 4-19-function-object/bReAk.js
@@ -0,0 +1,10 @@
+//break key word exploring
+//break = stop
+let i = 1;
+while (i <= 20) {
+  if (i == 10) {
+    break;
+  }
+  console.log(i);
+  i++;
+}
---
+++ 4-19-function-object/cOnTiNuE.js
@@ -0,0 +1,8 @@
+//continue keyword exploring
+//continue = skip
+for (let i = 1; i <= 30; i++) {
+  if (i >= 10 && i <= 20) {
+    continue;
+  }
+  console.log(i);
+}
