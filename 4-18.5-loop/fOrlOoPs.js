a041a7aa9369128e7e8a4142059f8d16792cac04
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 10:44:17 +0600

    explore for loop

--- 4-18.5-loop/fOrlOoPs.js
+++ 4-18.5-loop/fOrlOoPs.js
@@ -1,1 +1,20 @@
-// odd /even and print 1 to 10
+//  print 1 to 10 natural numbers
+console.log("1 to 10 Natural Numbers");
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+//  print 1 to 10 odd numbers
+console.log("1 to 10 Odd Numbers");
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
+
+//  print 1 to 10 even numbers
+console.log("1 to 10 Even Numbers");
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
