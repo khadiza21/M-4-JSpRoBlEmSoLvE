f2bb2a4df202c2e736a2ab0312ac02b980478a7b
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 10:23:36 +0600

    traffic signal command check with conditions

--- 4-18-array-condition/trafficSignal.js
+++ 4-18-array-condition/trafficSignal.js
@@ -1,1 +1,19 @@
-/* তাড়াহুড়া করে স্কুলের জন্য বের হচ্ছ কিন্তু রাস্তা পার হওয়ার সময় দেখলে, ট্রাফিক সিগন্যাল লাল রং। এই অবস্থায় যদি তুমি রাস্তা পার হওয়ার চেষ্টা কর, তাহলে ডেঞ্জার হতে পারে। যদি হলুদ রং হয় তাহলে তোমার থেমে যাওয়া উচিত। আর যদি ট্রাফিক সিগন্যাল গ্রিন হয় তাহলে তোমার রাস্তা পার হওয়া উচিত। তাই একটা কোড লিখে ফেলো। যেখানে আমরা signal নামে একটা ভেরিয়েবল থাকবে। সেটার মান green, yellow বা red হতে পারে। সেই অনুসারে ডিফারেন্ট ডিফারেন্ট কাজ হবে। তো, সেই কোড ফটাফট লিখে ফেলো */
+//traffic signal command check
+function trafficSignal(signal) {
+  if (signal == "red") {
+    return "RED! Danger! You Cann't Cross The Road!";
+  } else if (signal == "yellow") {
+    return "Yellow! You Should Stop!";
+  } else if (signal == "green") {
+    return "Green! You Can Cross The Road!";
+  } else {
+    return "Enter Valid Color.";
+  }
+}
+// let s = 'ReD';
+// console.log(s.toLowerCase());
+
+console.log(trafficSignal("ReD".toLowerCase()));
+console.log(trafficSignal("ORaNGe".toLowerCase()));
+console.log(trafficSignal("gReEn".toLowerCase()));
+console.log(trafficSignal("YeLlOw".toLowerCase()));
