8ccdb836d66002a23dcf77b6df2cf05ba1a34f17
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 15:32:18 +0600

    summetion of 3 values

--- 4-20.5-pS/sum3values.js
+++ 4-20.5-pS/sum3values.js
@@ -1,2 +1,6 @@
 /* তিনটা সংখ্যা এর যোগ করতে 
 পারবে এমন একটা ফাংশন লিখো  */
+function addThreeValues(number1,number2,number3){
+ console.log("Summtion of three valuesL ",number1+number2+number3);
+}
+addThreeValues(14,6,10);
