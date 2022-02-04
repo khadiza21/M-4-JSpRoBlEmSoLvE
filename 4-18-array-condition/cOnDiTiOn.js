6fc63b23f37776e1e3f2168c426ab7f298d3f16d
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 08:37:48 +0600

    condition

--- 4-18-array-condition/cOnDiTiOn.js
+++ 4-18-array-condition/cOnDiTiOn.js
@@ -1,2 +1,20 @@
-// >, <, ==, !=, <=, >=
-//  if(var == ture || var3>34), multistage and nested condition, 
+//  if(var == ture || var3>34), multistage and nested condition,
+
+// comparisom operator: >, <, ==, !=, <=, >=
+var num1 = 34;
+var num2 = 98;
+console.log("number 1 is less than number 2 ", num1 < num2);
+console.log("number 1 is greater than number 2 ", num1 > num2);
+console.log("number 1 is equal number 2 ", num1 == num2);
+console.log("number 1 is not equal number 2 ", num1 != num2);
+console.log("number 1 is less than and equal number 2 ", num1 <= num2);
+console.log("number 1 is greater than and equal number 2 ", num1 >= num2);
+console.log("true == 1: ", true == 1);
+console.log("false == 0: ", false == 0);
+
+//condition
+if (num2 == num1 || (num1 > 35 == 0 && num1 < 100)) {
+  console.log("condition");
+}
+
+//multistage condition
