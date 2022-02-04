19c43f0d9fcd055ebab8d5eec2d99506e92ab161
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 17:49:23 +0600

    smallest value find from 3 numbers

---
+++ 4-21-pS/largestValue.js
@@ -0,0 +1,11 @@
+function findLargeNumbers(num1, num2, num3) {
+  if (num1 > num2 && num1 > num3) {
+    console.log(num1, " is the leargest number.");
+  } else if (num2 > num1 && num2 > num3) {
+    console.log(num2, " is the leargest number.");
+  } else if (num3 > num1 && num3 > num2) {
+    console.log(num3, " is the leargest number.");
+  }
+}
+
+findLargeNumbers(4, 8, 12);
--- 4-21-pS/smallestValue.js
+++ 4-21-pS/smallestValue.js
@@ -1,11 +1,11 @@
-function findSmallValue(num1, num2, num3) {
+function findSmallNumber(num1, num2, num3) {
   if (num1 < num2 && num1 < num3) {
-    console.log(num1 + " is the smallest element.");
+    console.log(num1, " is the smallest number.");
   } else if (num2 < num1 && num2 < num3) {
-    console.log(num2 + " is the smallest element.");
+    console.log(num2, " is the smallest number.");
   } else if (num3 < num1 && num3 < num2) {
-    console.log(num3 + " is the smallest element.");
+    console.log(num3, " is the smallest number.");
   }
 }
 
-findSmallValue(4, 8, 12);
+findSmallNumber(4, 8, 12);
