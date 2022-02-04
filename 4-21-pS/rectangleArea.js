2406a1553e85a6dc6b06d3d4efd3998e83010310
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 17:23:59 +0600

    rectangle area calculating

--- 4-21-pS/rectangleArea.js
+++ 4-21-pS/rectangleArea.js
@@ -1,8 +1,8 @@
 /* ৪. একটা ফাংশন লিখো। যেটা ইনপুট প্যারামিটার হিসেবে একটা আয়তক্ষেত্রের দৈর্ঘ্য আর উচ্চতাকে নিবে। 
 তারপর সেই আয়তক্ষেত্র এর area (আয়তন) কে রেজাল্ট হিসেবে রিটার্ন করবে।  */
 
-function rectangleArea(length,width) {
-  let area = length*width;
+function rectangleArea(length, width) {
+  let area = length * width;
   return area;
 }
-console.log("Rectangle Area ", rectangleArea(34,31));
+console.log("Rectangle Area ", rectangleArea(34, 31));
