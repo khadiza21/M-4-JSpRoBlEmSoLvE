65336504d18707ee5e461fc168032b22347cb582
khadiza21 <bibikhadiza474@gmail.com>
2022-02-04 17:44:24 +0600

    smallest value find from 3 numbers

--- 4-21-pS/smallestValue.js
+++ 4-21-pS/smallestValue.js
@@ -1,13 +1,11 @@
-function findSmallValue(num1,num2,num3){
-       if(num1<num2 && num1<num3){
-           console.log(num1 + " is the smallest element.");
-       }
-       else if (num2<num1 && num2<num3){
-        console.log(num2 + " is the smallest element.");
-       }
-       else{
-        console.log(num3 + " is the smallest element.");
-       }
+function findSmallValue(num1, num2, num3) {
+  if (num1 < num2 && num1 < num3) {
+    console.log(num1 + " is the smallest element.");
+  } else if (num2 < num1 && num2 < num3) {
+    console.log(num2 + " is the smallest element.");
+  } else if (num3 < num1 && num3 < num2) {
+    console.log(num3 + " is the smallest element.");
+  }
 }
 
-findSmallValue(234,98,12);
+findSmallValue(4, 8, 12);
