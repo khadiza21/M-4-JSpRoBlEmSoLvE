016531500614c696fccae47ae501afb4591764ac
khadiza21 <bibikhadiza474@gmail.com>
2022-02-05 18:14:43 +0600

    centemeter to meter

--- 4-22.5-pS/centemeterToMeter.js
+++ 4-22.5-pS/centemeterToMeter.js
@@ -1,5 +1,5 @@
-function centemeterToMeter(cm){
-    let meter = cm/100;
-    return meter;
+function centemeterToMeter(cm) {
+  let meter = cm / 100;
+  return meter;
 }
 console.log(centemeterToMeter(200));
