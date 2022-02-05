f1b2ae2c69ea483a9b6385eb761293c202dbffbf
khadiza21 <bibikhadiza474@gmail.com>
2022-02-05 18:09:07 +0600

    centemeter to feet

--- 4-22.5-pS/centemeterToMeter.js
+++ 4-22.5-pS/centemeterToMeter.js
@@ -1,5 +1,5 @@
-// function centemeterToMeter(cm){
-//     let meter = cm/100;
-//     return meter;
-// }
-// console.log(centemeterToMeter(5));
+function centemeterToMeter(cm){
+    let meter = cm/100;
+    return meter;
+}
+console.log(centemeterToMeter(5));
