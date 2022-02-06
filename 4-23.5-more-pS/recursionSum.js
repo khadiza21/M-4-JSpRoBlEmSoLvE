fee27ceb63f9a1fb8e30e580c81dfa4b73231c16
khadiza21 <bibikhadiza474@gmail.com>
2022-02-06 11:24:50 +0600

    recursion sum

--- 4-23.5-more-pS/recursionSum.js
+++ 4-23.5-more-pS/recursionSum.js
@@ -4,10 +4,17 @@
 // }
 // console.log(sum);
 
-function sum(i){
-    if(i==1){
-        return 1;
-    }
-    return i+ sum(i-1);
+function sum(i) {
+  if (i == 1) {
+    return 1;
+  }
+  return i + sum(i - 1);
 }
 console.log(sum(5));
+
+// i + sum(--i)
+// 5 + sum(4)
+// 5 + 4 + sum(3)
+// 5 + 4 + 3 + sum(2)
+// 5 + 4 + 3 + 2 + sum(1)
+// 5 + 4 + 3 + 2 + 1
