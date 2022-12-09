3a41eeb607cc69075270a9a4994eec921ff539a0
khadiza21 <bibikhadiza474@gmail.com>
2022-12-09 20:19:52 +0600

    IIFEs function and fun expressionn

---
+++ recap/IIFEsFunction.js
@@ -0,0 +1,10 @@
+// IIFEs (Immediately Invokeable Function Expression)
+(function display(message){
+  console.log(message);
+  console.log(" bk");
+})("hello");
+
+//function expression
+const display2 = function displayMessage(){
+console.log("la tahjan");
+}
